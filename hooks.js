49274446e41ee9e0d0c43ae0752201560ea43d4b
soyuka <soyuka@gmail.com>
2015-09-20 13:14:24 +0200

    fix routes for the new ap

--- hooks.js
+++ hooks.js
@@ -2,7 +2,7 @@ function registerHooks(config) {
   return {
     element: function(item) {
       if(item.ext == '.sfv') {
-        return '<a href="/plugin/cksfv?path='+item.path+'">Cksfv</a>'
+        return '<a href="/p/cksfv?path='+item.path+'">Cksfv</a>'
       }
 
       return ''
--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 var CksfvRouter = function(app, utils) {
-  app.get('/plugin/cksfv', utils.prepareTree, function(req, res, next) {
+  app.get('/', utils.prepareTree, function(req, res, next) {
     utils.interactor.ipc.send('call', 'cksfv.check', req.user, req.query.path)
 
     return res.handle('back', {info: 'Cksfv launched'}, 201)
